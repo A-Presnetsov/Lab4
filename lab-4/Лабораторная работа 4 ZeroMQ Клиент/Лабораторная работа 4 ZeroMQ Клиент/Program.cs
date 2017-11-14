76d761dd8c99f2517da02125f9e1760d4cba615c
A-Presnetsov <33604502+A-Presnetsov@users.noreply.github.com>
2017-11-14 19:28:01 +0300

    Add files via upload

---
+++ lab-4/Лабораторная работа 4 ZeroMQ Клиент/Лабораторная работа 4 ZeroMQ Клиент/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroMQ;
+
+namespace Лабораторная_работа_4_ZeroMQ_Клиент
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZContext c = new ZContext();
+            ZSocket s = new ZSocket(c, ZSocketType.PULL);
+            s.Connect("tcp://127.0.0.1:1918");
+            while (true)
+            {
+                ZFrame frame = s.ReceiveFrame();
+                Console.WriteLine("Сервер прислал строку: " + frame.ReadString());
+            }
+        }
+    }
+}
---
+++ lab-4/Лабораторная работа 4 ZeroMQ/Лабораторная работа 4 ZeroMQ/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroMQ;
+
+namespace Лабораторная_работа_4_ZeroMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new ZContext();
+            var s = new ZSocket(c, ZSocketType.PUSH);
+            s.Bind("tcp://127.0.0.1:1918");
+            Console.WriteLine("Сервер запущен.");
+            while (true)
+            {
+                Console.Write("Введите строку: ");
+                var message = Console.ReadLine();
+                s.SendFrame(new ZFrame(message));
+            }
+        }
+    }
+}
